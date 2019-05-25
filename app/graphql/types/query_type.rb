9fb83bcf560b66bc3b0d989d064ed0e0a347b644
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2019-05-25 21:46:20 +0900

    first commit

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    field :articles, Types::ArticleType.connection_type, null: false, resolve: -> (_object, _args, _context) do
-      Article.recent
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World!"
     end
   end
 end
